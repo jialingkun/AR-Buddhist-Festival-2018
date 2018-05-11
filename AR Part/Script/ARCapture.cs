59b0676615156da0541327bc8629b537cbebb38e
jialingkun <djaka.tingkir2@gmail.com>
2018-05-11 15:47:54 +0700

    Camera save load flash

--- AR Part/Script/ARCapture.cs
+++ AR Part/Script/ARCapture.cs
@@ -5,19 +5,31 @@ using UnityEngine.UI;
 using System.IO;
 
 public class ARCapture : MonoBehaviour {
-	GameObject captureButton;
-	Text errorText;
+	private GameObject captureButton;
+	private Text errorText;
+
+	//flash
+	private Image flashPanel;
+	public float FlashIntensity = 0.7f;
+	public float FlashDuration = 0.7f;
+
 	// Use this for initialization
 	void Start () {
+		Screen.orientation = ScreenOrientation.AutoRotation;
 		captureButton = GameObject.Find ("Capture");
 		errorText = GameObject.Find ("Error").GetComponent<Text>();
+		flashPanel = GameObject.Find ("FlashPanel").GetComponent<Image>();
+		flashPanel.CrossFadeAlpha (0.0f, 0.1f, false);
+
+		//Load
+		SaveLoad.loadImageName(); //temporary, should be execute at start menu
+		print (SaveLoad.imageNameList.Count);
 
 		// Get a reference to the storage service, using the default Firebase App
-		Firebase.Storage.FirebaseStorage storage = Firebase.Storage.FirebaseStorage.DefaultInstance;
+		//Firebase.Storage.FirebaseStorage storage = Firebase.Storage.FirebaseStorage.DefaultInstance;
 
 		// Create a storage reference from our storage service
-		Firebase.Storage.StorageReference storage_ref =
-			storage.GetReferenceFromUrl("gs://buddhist-festival-ar-2018.appspot.com");
+		//Firebase.Storage.StorageReference storage_ref = storage.GetReferenceFromUrl("gs://buddhist-festival-ar-2018.appspot.com");
 	}
 	
 	// Update is called once per frame
@@ -29,14 +41,19 @@ public class ARCapture : MonoBehaviour {
 		captureButton.SetActive (false);
 		yield return new WaitForEndOfFrame();
 		//take screen shot
+
 		Texture2D screenTexture = new Texture2D(Screen.width, Screen.height,TextureFormat.RGB24,true);
 		screenTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, true);
 		screenTexture.Apply();
 
+		//flash
+		flashPanel.CrossFadeAlpha (1.0f, 0.1f, false);
+		yield return new WaitForSeconds (0.1f);
+
 		//save screen shot
 		byte[] dataToSave = screenTexture.EncodeToPNG();
 		string destination = Application.persistentDataPath + "/../../../../Pictures/ARBuddhistFestival2018";
-		string filename = "test.png";
+		string filename = System.DateTime.Now.ToString("yyyymmdd_hhmmss")+".png";
 		try {
 			if (!Directory.Exists(destination))
 			{
@@ -44,9 +61,12 @@ public class ARCapture : MonoBehaviour {
 			}
 
 			File.WriteAllBytes(destination+ "/" + filename, dataToSave);
+			SaveLoad.saveImageName(filename);
 		} catch (System.Exception ex) {
 			errorText.text = ex.ToString();
 		}
+		flashPanel.CrossFadeAlpha (0.0f, FlashDuration, false);
+		yield return new WaitForSeconds (FlashDuration-0.1f);
 		//errorText.text = "Finished Capturing.. Uploading...";
 		captureButton.SetActive (true);
 
---
+++ AR Part/Script/SaveLoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveLoad{
+	public static List<string> imageNameList;
+
+	public static void loadImageName(){
+		string tempImageList = PlayerPrefs.GetString("ImageName","").Trim();
+		if (tempImageList == "") {
+			imageNameList = new List<string> ();
+		} else {
+			imageNameList = new List<string> (tempImageList.Split(","[0]));
+		}
+	}
+
+	public static void saveImageName(string filename){
+		imageNameList.Add (filename);
+		PlayerPrefs.SetString("ImageName",string.Join(",", imageNameList.ToArray()));
+	}
+}
